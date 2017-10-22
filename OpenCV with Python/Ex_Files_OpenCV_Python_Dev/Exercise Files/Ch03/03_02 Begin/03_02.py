1e7083095544839311c037f671307d9a50011ef5
vitaliAI <vitali_mueller@yahoo.de>
2017-10-22 18:57:06 +0100

    Binary Threshold

--- OpenCV with Python/Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
+++ OpenCV with Python/Ex_Files_OpenCV_Python_Dev/Exercise Files/Ch03/03_02 Begin/03_02.py
@@ -1,3 +1,35 @@
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
+# Simple Thresholding
+
+bw = cv2.imread('detect_blob.png',0)
+
+plt.imshow(bw)
+
+height, width = bw.shape
+
+cv2.imshow('Original BW', bw)
+
+binary = np.zeros([height, width,1], 'uint8')
+
+threshold = 85
+
+for row in range(0, height):
+    for col in range(0,width):
+        if bw[row][col] > threshold:
+            binary[row][col] = 255
+            
+            
+
+ret, thresh = cv2.threshold(bw, threshold, 255, cv2.THRESH_BINARY)
+plt.imshow(thresh)
+
+cv2.imshow('CV Threshold', thresh)
+
+cv2.imshow('Slow Binary', binary)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
